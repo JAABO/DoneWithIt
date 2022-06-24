3db578ebc687a0b4e9495f06c85ff6f150eb383c
Jake Boldbaatar <jaagaa@icloud.com>
2022-06-23 21:30:30 -0500

    AccountScreen-created

--- App.js
+++ App.js
@@ -1,22 +1,8 @@
-import { React } from 'react';
-import {View} from 'react-native';
-import AppText from './app/component/AppText/AppText';
-
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
-import ViewImageScreen from './app/screens/ViewImageScreen';
-import WelcomeScreen from './app/screens/WelcomeScreen';
-import Card from './app/component/Card'
-import MessagesScreen from './app/screens/MessagesScreen';
-// import {Welcomescreen} from "./screens";
+import { React } from "react";
+import { View } from "react-native";
 
 export default function App() {
-  console.log("App executed.")
+  console.log("Hey Jake, now code compiled and App is running.");
 
-  return (
-    <MessagesScreen/>
-  // <ListingDetailsScreen/>
-  // <ViewImageScreen/>
-  // <WelcomeScreen/>
-
-  );
+  return;
 }
